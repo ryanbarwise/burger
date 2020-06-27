7a537b634c2f7b841f82bed210fa987eb1d75435
Ryan Barwise <ryanbarwise@gmail.com>
2020-06-27 14:00:07 -0400

    fixed delete click issue

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,59 +1,44 @@
-var express = require("express");
+var express = require('express');
 
-var burger = require("../models/burger.js");
-
+var burger = require('../models/burger.js');
 
 var router = express.Router();
 
-
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+router.get('/', function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
   });
+});
 
-  router.post("/api/burgers", function(req, res){
-    burger.insertOne([
-       "burger_name"
-    ], [
-      req.body.burger_name
-    ], function(result){
-      
-      res.json({ id: result.insertID})
-    });
+router.post('/api/burgers', function (req, res) {
+  burger.insertOne(['burger_name'], [req.body.burger_name], function (result) {
+    res.json({ id: result.insertID });
   });
+});
 
+router.put('/api/burgers/:id', function (req, res) {
+  burger.updateOne(
+    { devoured: req.body.devoured },
+    `id = ${req.params.id}`,
+    function (result) {
+      res.json({ id: req.params.id });
+    }
+  );
+});
 
-  router.put("/api/burgers/:id", function(req, res){
-    burger.updateOne({devoured: req.body.devoured}, `id = ${req.params.id}`, function(result){
-      res.json({ id: req.params.id})
-    });
+router.delete('/api/burgers/:id', function (req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  burger.delete(condition, function (result) {
+    // if (result.affectedRows == 0) {
+    //   res.json({ id: req.params.id });
+    // }
+    res.end();
   });
-
-
-  router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    burger.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-      res.json({id: req.params.id})
-      }
-    });
-  });
-   
-
-  
-
-
-
-
-
-
-
-
+});
 
 module.exports = router;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,60 +1,53 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-devour").on("click", function(event) {
-    var id = $(this).data("id");
-    var devoured = $(this).data("devoured");
 
-    var newDevouredState = {
-      devoured: !devoured
-    };
+$('.change-devour').on('click', function (event) {
+  var id = $(this).data('id');
+  var devoured = $(this).data('devoured');
 
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevouredState
-    }).then(
-      function() {
-        console.log("changed devoured to", !devoured);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+  var newDevouredState = {
+    devoured: !devoured,
+  };
+
+  // Send the PUT request.
+  $.ajax('/api/burgers/' + id, {
+    type: 'PUT',
+    data: newDevouredState,
+  }).then(function () {
+    console.log('changed devoured to', !devoured);
+    // Reload the page to get the updated list
+    location.reload();
   });
+});
 
-  $(".add-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+$('.add-form').on('submit', function (event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
 
-    var newBurger = {
-      burger_name: $("#burg").val().trim(),
-      // sleepy: $("[name=sleepy]:checked").val().trim()
-    };
+  var newBurger = {
+    burger_name: $('#burg').val().trim(),
+    // sleepy: $("[name=sleepy]:checked").val().trim()
+  };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+  // Send the POST request.
+  $.ajax('/api/burgers', {
+    type: 'POST',
+    data: newBurger,
+  }).then(function () {
+    console.log('created new burger');
+    // Reload the page to get the updated list
+    location.reload();
   });
+});
 
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+$('.delete-burger').on('click', function (event) {
+  var id = $(this).data('id');
+  console.log('clicked');
+  // Send the DELETE request.
+  $.ajax('/api/burgers/' + id, {
+    type: 'DELETE',
+  }).then(function () {
+    console.log('deleted burger', id);
+    // Reload the page to get the updated list
+    location.reload();
   });
 });
